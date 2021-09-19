25f386c139b7b113ea3b4cdea52926a9fe73bcea
Kallyas <kallyasmedia@gmail.com>
2021-09-19 13:09:55 +0300

    configure endpoints

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->get('users', ['uses' => 'UserController@showAllUsers']);
+    $router->get('users/{id}', ['uses' => 'UserController@showOneUser']);
+
+    $router->post('auth/signup', ['uses' => 'UserController@createUser']);
+
+    $router->delete('users', ['uses' => 'UserController@deleteUser']);
+
+    $router->put('users/{id}', ['uses' => 'UserController@updateUser']);
+});
